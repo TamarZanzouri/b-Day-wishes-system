2b78b47aa1fe5f3cdd252db97bd3e4d155de7cdf
Tamar Zanzouri <zanzouritamar@gmail.com>
2015-07-19 19:12:21 +0300

    merge

--- app/public/js/users.js
+++ app/public/js/users.js
@@ -312,6 +312,7 @@ app.directive("outsideClick", ['$document', function( $document){
 
         $scope.showReminer = function(index){
             $scope.direction = 'left';
+            console.log(index);
             if($('#' + index + '>img.moveToArcive').hasClass('active')){
                 $('#' + index + '>img.moveToArcive').removeClass('active');
                 $('#' + index + ' div.friend-img').css("display", "block");
@@ -463,7 +464,7 @@ app.directive("outsideClick", ['$document', function( $document){
                 }
                 return -1;
             }
-            else if(parseInt(date[0])-parseInt(datetime[0]) == 1){
+            else if(parseInt(date[1])-parseInt(datetime[1]) == 1){
 
                 if(parseInt(date[0])+30-parseInt(datetime[0])<=7){
                     return 2;
@@ -479,6 +480,7 @@ app.directive("outsideClick", ['$document', function( $document){
 
 
         $scope.addReminder = function($event, user, index){
+
             user.BirthdayReminderFlag = true;
             $http.post(domain + '/updateReminderFlag' , 
                 {friendName : user.friendName, userEmail : User.userEmail}).success(function(data){
